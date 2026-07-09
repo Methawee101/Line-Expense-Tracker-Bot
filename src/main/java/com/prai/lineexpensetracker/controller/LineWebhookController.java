f81b913058766d3278e9847ad1fc3ec870b2aa62
Methawee101 <pray.methawee@gmail.com>
2026-07-09 21:36:23 +0700

    lineWebHook message send Income/Expense/SummaryMonthly

--- src/main/java/com/prai/lineexpensetracker/controller/LineWebhookController.java
+++ src/main/java/com/prai/lineexpensetracker/controller/LineWebhookController.java
@@ -3,6 +3,8 @@ package com.prai.lineexpensetracker.controller;
 import com.prai.lineexpensetracker.dto.line.lineEvent;
 import com.prai.lineexpensetracker.dto.line.lineWebhookRequest;
 import com.prai.lineexpensetracker.dto.request.transactionRequest;
+import com.prai.lineexpensetracker.dto.response.monthlySummaryResponse;
+import com.prai.lineexpensetracker.dto.response.transactionResponse;
 import com.prai.lineexpensetracker.enums.TypeTransaction;
 import com.prai.lineexpensetracker.service.TransactionService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/webhook/line")
@@ -20,9 +25,10 @@ public class LineWebhookController {
     private final TransactionService transactionService;
 
     @PostMapping
-    public void handleWebhook(@RequestBody lineWebhookRequest request){
+    public List<String> handleWebhook(@RequestBody lineWebhookRequest request){
+        List<String> responses = new ArrayList<>();
         if(request.getEvents() == null){
-            return;
+            return responses;
         }
 
         for (lineEvent event: request.getEvents()){
@@ -38,13 +44,107 @@ public class LineWebhookController {
             String lineUserId = event.getSource().getUserId();
             String text = event.getMessage().getText();
 
-            transactionRequest transactionRequest = parseTextToRequest(lineUserId,text);
+            String responseMessage = handleTextMessage(lineUserId, text);
+            responses.add(responseMessage);
+//            transactionRequest transactionRequest = parseTextToRequest(lineUserId,text);
 
-            transactionService.createTransaction(transactionRequest);
+
+//            transactionService.createTransaction(transactionRequest);
 
         }
+        return responses;
 
     }
+
+    private String handleTextMessage(String lineUserId, String text) {
+        String trimText = text.trim();
+
+        if(trimText.startsWith("รายรับ") || trimText.startsWith("รายจ่าย")){
+            return handleCreateTransaction(lineUserId,trimText);
+        }
+
+        if(trimText.equals("สรุปเดือนนี้")){
+            return handleMonthSummary(lineUserId);
+        }
+        if (trimText.startsWith("สรุปเดือน")){
+            return handleSpecificMonthSummary(lineUserId,text);
+        }
+
+        return """
+                รูปแบบข้อความไม่ถูกต้อง
+                
+                ตัวอย่าง:
+                รายรับ เงินเดือน 15000
+                รายจ่าย กาแฟ 50
+                
+                สรุปเดือนนี้
+                สรุปเดือน 2026-07
+                """;
+
+
+    }
+
+//    รายรับรายจ่าย
+    private String handleCreateTransaction(String lineUserId, String text) {
+        transactionRequest request = parseTextToRequest(lineUserId, text);
+
+        transactionResponse response = transactionService.createTransaction(request);
+
+        String typeText = response.getType() == TypeTransaction.INCOME
+                ? "รายรับ"
+                : "รายจ่าย";
+
+        return String.format(
+                "บันทึกเรียบร้อย ✅%n%s: %s%nจำนวน: %s บาท",
+                typeText,
+                response.getTitle(),
+                response.getAmount()
+        );
+    }
+
+//    กรณีสรุปรายเดือนนี้
+    private String handleMonthSummary(String lineUserID) {
+        YearMonth currentMonth = YearMonth.now();
+
+        monthlySummaryResponse summary = transactionService.getMonthlySummary(lineUserID,currentMonth);
+
+        return formatSummaryMessage(summary);
+
+    }
+
+//    สรุประบุเดือน
+    private String handleSpecificMonthSummary(String lineUserId, String text){
+        String[] parts = text.trim().split("\\s+");
+
+        if (parts.length < 2 ){
+            return "กรุณาระบุเดือนเช่น สรุปเดือน 2026-07";
+        }
+
+        try{
+            YearMonth month = YearMonth.parse(parts[1]);
+            monthlySummaryResponse summary = transactionService.getMonthlySummary(lineUserId,month);
+            return formatSummaryMessage(summary);
+        }catch (Exception e){
+            return "รูปแบบเดือนไม่ถูกต้อง";
+        }
+    }
+
+    private String formatSummaryMessage(monthlySummaryResponse summary) {
+        return String.format(
+                """
+                สรุปรายจ่าย เดือน %s
+                
+                รายรับรวม: %s บาท
+                รายจ่ายรวม: %s บาท
+                คงเหลือ: %s บาท
+                        """,
+                summary.getMonth(),
+                summary.getTotalIncome(),
+                summary.getTotalExpense(),
+                summary.getBalance()
+        );
+    }
+
 
     private  transactionRequest parseTextToRequest(String lineUserId, String text) {
         String[] parts = text.trim().split("\\s+");
@@ -79,4 +179,5 @@ public class LineWebhookController {
 
         return request;
     }
+
 }
