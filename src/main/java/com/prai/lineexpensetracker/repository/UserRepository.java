309df76ff3549d10049a10af0da87ced717bb6a6
Methawee101 <pray.methawee@gmail.com>
2026-07-13 20:16:35 +0700

    scheduler monthlySummary Every 1st of month

--- src/main/java/com/prai/lineexpensetracker/repository/UserRepository.java
+++ src/main/java/com/prai/lineexpensetracker/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.prai.lineexpensetracker.repository;
 
 import com.prai.lineexpensetracker.entity.User;
+import com.prai.lineexpensetracker.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLineUserId(String lineUserId);
+    List<User> findByStatus(UserStatus status);
 }
---
+++ src/main/java/com/prai/lineexpensetracker/scheduler/monthlySummaryScheduler.java
@@ -0,0 +1,55 @@
+package com.prai.lineexpensetracker.scheduler;
+
+import com.prai.lineexpensetracker.dto.response.monthlySummaryResponse;
+import com.prai.lineexpensetracker.entity.User;
+import com.prai.lineexpensetracker.enums.UserStatus;
+import com.prai.lineexpensetracker.repository.UserRepository;
+import com.prai.lineexpensetracker.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.YearMonth;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class monthlySummaryScheduler {
+    private final UserRepository userRepository;
+    private final TransactionService transactionService;
+
+//    ทุกวันที่ 1
+    @Scheduled(cron = "0 0 9 1 * *")
+//    @Scheduled(cron = "*/30 * * * * *")
+    public  void sendMonthlySummary() {
+        YearMonth previousMonth = YearMonth.now().minusMonths(1);
+
+//   test     YearMonth previousMonth = YearMonth.now();
+        List<User> activeUsers = userRepository.findByStatus(UserStatus.ACTIVE);
+
+        for(User user : activeUsers) {
+            monthlySummaryResponse summary = transactionService.getMonthlySummary(user.getLineUserId(), previousMonth);
+
+            String message = formatSummaryMessage(summary);
+
+            // รอเชื่อม line
+            System.out.println("Send monthly summary to: " + user.getLineUserId());
+            System.out.println(message);
+        }
+    }
+    private String formatSummaryMessage(monthlySummaryResponse summary) {
+        return String.format(
+                """
+                สรุปรายรับรายจ่ายประจำเดือน %s
+                
+                รายรับรวม: %s บาท
+                รายจ่ายรวม: %s บาท
+                คงเหลือ: %s บาท
+                """,
+                summary.getMonth(),
+                summary.getTotalIncome(),
+                summary.getTotalExpense(),
+                summary.getBalance()
+        );
+    }
+}
